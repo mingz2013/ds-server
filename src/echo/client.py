edea268087c411a6e54516facb89507e784f524d
mingz <305603665@qq.com>
2017-06-10 09:33:41 +0800

    init

---
+++ src/echo/client.py
@@ -0,0 +1,30 @@
+# -*- coding:utf-8 -*-
+'''
+Created on 10/06/2017
+
+@author: zhaojm
+'''
+
+from sys import stdout
+
+from twisted.internet.protocol import Protocol, ClientFactory
+
+
+class Echo(Protocol):
+    def dataReceived(self, data):
+        stdout.write(data)
+
+
+class EchoClientFactory(ClientFactory):
+    def startedConnecting(self, connector):
+        print('Started to connect.')
+
+    def buildProtocol(self, addr):
+        print('Connected.')
+        return Echo()
+
+    def clientConnectionLost(self, connector, reason):
+        print('Lost connection.  Reason:', reason)
+
+    def clientConnectionFailed(self, connector, reason):
+        print('Connection failed. Reason:', reason)
---
+++ src/echo/server.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+'''
+Created on 10/06/2017
+
+@author: zhaojm
+'''
+
+from twisted.internet import protocol, reactor
+
+
+class Echo(protocol.Protocol):
+    def dataReceived(self, data):
+        self.transport.write(data)
+
+
+class EchoFactory(protocol.Factory):
+    def buildProtocol(self, addr):
+        return Echo()
+
+
+echo = EchoFactory()
+reactor.listenTCP(8888, echo)
