212fe347c7a7023a0bde01fb4e2e4fe62ab740fe
mingz <305603665@qq.com>
2017-06-15 14:07:21 +0800

    init

---
+++ src/chat_with_user/chat/client.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+'''
+Created on 15/06/2017
+
+@author: zhaojm
+'''
+
+
+class Client(object):
+    def __init__(self):
+        pass
---
+++ src/chat_with_user/chat/server.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+'''
+Created on 15/06/2017
+
+@author: zhaojm
+'''
+
+
+class Server(object):
+    def __init__(self):
+        pass
