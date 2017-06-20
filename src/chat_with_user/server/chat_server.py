7f88d29368736921f79c09f43d8421ff7705986f
mingz <305603665@qq.com>
2017-06-20 18:15:35 +0800

    init

--- src/chat_with_user/server/chat_server.py
+++ src/chat_with_user/server/chat_server.py
@@ -13,6 +13,12 @@ class ChatServer(BaseServer):
         BaseServer.__init__(self)
         pass
 
+    def on_conn_lost(self, conn, reason):
+        pass
+
+    def on_conn_made(self, conn):
+        pass
+
     def on_msg(self, conn, msg):
         conn.sendLine(msg)
 
