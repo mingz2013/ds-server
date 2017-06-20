1274e4aa54991010ec6ea8a45e74f27b63254175
mingz <305603665@qq.com>
2017-06-20 19:26:47 +0800

    init

--- frame/src/frame/core/factories.py
+++ frame/src/frame/core/factories.py
@@ -14,6 +14,7 @@ class BaseFactory(Factory):
         self.entity = entity
 
     def buildProtocol(self, addr):
+        # return self.protocol(self.entity)
         return BaseProtocol(self.entity)
 
 
