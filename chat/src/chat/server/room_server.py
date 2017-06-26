3a3e5c393d9e7c648528a1845b6511940fc6be24
mingz <305603665@qq.com>
2017-06-26 20:30:43 +0800

    init

--- chat/src/chat/server/room_server.py
+++ chat/src/chat/server/room_server.py
@@ -4,3 +4,20 @@ Created on 26/06/2017
 
 @author: zhaojm
 '''
+from frame.entity.base_server import BaseServer
+import g
+
+
+class RoomServer(BaseServer):
+    def __init__(self):
+        BaseServer.__init__(self)
+
+    def on_conn_lost(self, conn, reason):
+        pass
+
+    def on_conn_made(self, conn):
+        pass
+
+    def on_msg(self, conn, msg):
+        conn.sendLine(msg)
+        pass
