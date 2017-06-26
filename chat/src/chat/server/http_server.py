02ab0fe23602b389096dd7becfb86fc3600b88fd
mingz <305603665@qq.com>
2017-06-26 21:46:48 +0800

    init

--- chat/src/chat/server/http_server.py
+++ chat/src/chat/server/http_server.py
@@ -12,6 +12,8 @@ Created on 26/06/2017
 @author: zhaojm
 '''
 from frame.entity.base_server import BaseServer
+from flask import Flask
+from twisted.internet import reactor
 
 
 class HttpServer(BaseServer):
--- frame/src/frame/core/reactor.py
+++ frame/src/frame/core/reactor.py
@@ -7,7 +7,13 @@ Created on 19/06/2017
 
 import stackless
 from twisted.internet import reactor, stdio
-
+# from twisted.web.resource import Resource
+# from twisted.web.proxy import ReverseProxyResource
+# from twisted.application import internet, service, strports
+# from twisted.web import static, server
+# from twisted.web.resource import Resource
+# from twisted.web.server import Site
+from twisted.web.wsgi import WSGIResource
 from factories import BaseFactory, BaseClientFactory
 from protocols import StandardIOProtocol
 
@@ -16,8 +22,13 @@ def init_server(entity, ip, port):
     reactor.listenTCP(port, BaseFactory(entity))
 
 
-def init_http_server():
-    pass
+def init_http_server(app):
+    # root = Resource()
+    flask_site = WSGIResource(reactor, reactor.getThreadPool(), app)
+    # root.putChild('api', flask_site)
+    # static_file = static.File("webroot")
+    # root.putChild('static', static_file)
+    reactor.listenTCP(80, flask_site)
 
 
 def init_ws_server():
