c3c2a6cabb92bd09d7868f956143e4972c4555d5
mingz <305603665@qq.com>
2017-06-26 21:11:53 +0800

    init

---
+++ chat/src/chat/server/sio_server.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+'''
+Created on 26/06/2017
+
+@author: zhaojm
+'''
+
+from frame.entity.base_server import BaseServer
+
+
+class SIOServer(BaseServer):
+    def __init__(self):
+        BaseServer.__init__(self)
+
+    def on_conn_lost(self, conn, reason):
+        pass
+
+    def on_conn_made(self, conn):
+        pass
+
+    def on_msg(self, conn, msg):
+        conn.sendLine(msg)
+        pass
