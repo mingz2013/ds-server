2a6515e8a3943a1bf18e81e54bee20b9f52d277c
mingz <305603665@qq.com>
2017-07-31 05:00:08 +0800

    init

--- frame/src/frame/core/http.py
+++ frame/src/frame/core/http.py
@@ -1,9 +1,9 @@
-# -*- coding:utf-8 -*-
-"""
-"""
-__date__ = "31/07/2017"
-__author__ = "zhaojm"
-
-from flask import Flask
-
-app = Flask(__name__)
+# # -*- coding:utf-8 -*-
+# """
+# """
+# __date__ = "31/07/2017"
+# __author__ = "zhaojm"
+#
+# from flask import Flask
+#
+# app = Flask(__name__)
