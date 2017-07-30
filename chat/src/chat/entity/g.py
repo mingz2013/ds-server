c47cbd3f517b2bd6f8d7174552bb1c51bd5355b6
mingz <305603665@qq.com>
2017-07-30 22:00:14 +0800

    init

--- chat/src/chat/entity/g.py
+++ chat/src/chat/entity/g.py
@@ -10,10 +10,21 @@ Created on 21/06/2017
 s = {}  # 存储server对象列表, 限制了每个进程最多一个相同的server对象
 
 
-def main():
+def init_servers():
     s_list_str = ["db", "gate", "http", "manager", "proxy", "sdk", "sio", "ws"]
     for s_str in s_list_str:
         # register server
         exec "from chat.servers.%s.server import Server; s['%s'] = Server()" % s_str
         # register rpc
         exec "from chat.servers.%s.rpc import *" % s_str
+
+
+def start_servers():
+    from frame.core import reactor
+    for (k, v) in s.items():
+        if k in ["db", "gate", "manager", "proxy"]:
+            reactor.init_server(v, "127.0.0.1", 8888)
+        elif k in ["http", "sdk"]:
+            reactor.init_http_server(v, "127.0.0.1", 80)
+        else:
+            pass
