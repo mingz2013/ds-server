2c1efd4589f0e1d10b9936010ea221f336910187
mingz <305603665@qq.com>
2017-07-30 16:17:19 +0800

    init

--- chat/src/__demos__/chat_demo/server/run.py
+++ chat/src/__demos__/chat_demo/server/run.py
@@ -13,10 +13,10 @@ source_path = os.path.join(current_path, "../../../../")
 sys.path.append(os.path.join(source_path, "chat/src/"))
 sys.path.append(os.path.join(source_path, "frame/src/"))
 
-from chat_server import ChatServer
-from frame.core import reactor
-
-if __name__ == '__main__':
-    s = ChatServer()
-    reactor.init_server(s, '0.0.0.0', 8888)
-    reactor.start_reactor()
+# from chat_server import ChatServer
+# from frame.core import reactor
+#
+# if __name__ == '__main__':
+#     s = ChatServer()
+#     reactor.init_server(s, '0.0.0.0', 8888)
+#     reactor.start_reactor()
